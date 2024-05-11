e14b184070c569e94fa1e8dbff1db31264532ccb
Barillas <jbarillas-2017174@kinal.edu.gt>
2024-05-11 10:43:30 -0600

    reporte de usuarios

--- Reportes.h
+++ Reportes.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "Cstomer.h"
 
 namespace QuickShop {
 
@@ -7,13 +11,19 @@ namespace QuickShop {
 	using namespace System::Collections;
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
+	using namespace System::Drawing::Drawing2D;
 	using namespace System::Drawing;
+	using namespace System::Collections::Generic;
+	using namespace System::Runtime::InteropServices;
+	using namespace System::IO;
+	using namespace std;
 
 	/// <summary>
 	/// Resumen de Reportes
 	/// </summary>
 	public ref class Reportes : public System::Windows::Forms::Form
 	{
+	private: cli::array<Cstomer^>^ localData = gcnew cli::array<Cstomer^>(100);
 	public:
 		Reportes(void)
 		{
@@ -72,12 +82,12 @@ namespace QuickShop {
 			this->titlePage = (gcnew System::Windows::Forms::Label());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->panel_buttons = (gcnew System::Windows::Forms::Panel());
-			this->btn_clients = (gcnew System::Windows::Forms::Button());
-			this->btn_products = (gcnew System::Windows::Forms::Button());
-			this->btn_purchases = (gcnew System::Windows::Forms::Button());
-			this->btn_payments = (gcnew System::Windows::Forms::Button());
-			this->btn_inventary = (gcnew System::Windows::Forms::Button());
 			this->btn_users = (gcnew System::Windows::Forms::Button());
+			this->btn_inventary = (gcnew System::Windows::Forms::Button());
+			this->btn_payments = (gcnew System::Windows::Forms::Button());
+			this->btn_purchases = (gcnew System::Windows::Forms::Button());
+			this->btn_products = (gcnew System::Windows::Forms::Button());
+			this->btn_clients = (gcnew System::Windows::Forms::Button());
 			this->panel_table = (gcnew System::Windows::Forms::Panel());
 			this->dgv_report = (gcnew System::Windows::Forms::DataGridView());
 			this->panel_buttons->SuspendLayout();
@@ -117,45 +127,16 @@ namespace QuickShop {
 			this->panel_buttons->Size = System::Drawing::Size(666, 33);
 			this->panel_buttons->TabIndex = 2;
 			// 
-			// btn_clients
+			// btn_users
 			// 
-			this->btn_clients->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->btn_clients->Location = System::Drawing::Point(0, 4);
-			this->btn_clients->Name = L"btn_clients";
-			this->btn_clients->Size = System::Drawing::Size(75, 23);
-			this->btn_clients->TabIndex = 0;
-			this->btn_clients->Text = L"Clientes";
-			this->btn_clients->UseVisualStyleBackColor = true;
-			// 
-			// btn_products
-			// 
-			this->btn_products->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->btn_products->Location = System::Drawing::Point(81, 4);
-			this->btn_products->Name = L"btn_products";
-			this->btn_products->Size = System::Drawing::Size(75, 23);
-			this->btn_products->TabIndex = 1;
-			this->btn_products->Text = L"Catalogo";
-			this->btn_products->UseVisualStyleBackColor = true;
-			// 
-			// btn_purchases
-			// 
-			this->btn_purchases->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->btn_purchases->Location = System::Drawing::Point(162, 4);
-			this->btn_purchases->Name = L"btn_purchases";
-			this->btn_purchases->Size = System::Drawing::Size(75, 23);
-			this->btn_purchases->TabIndex = 2;
-			this->btn_purchases->Text = L"Compras";
-			this->btn_purchases->UseVisualStyleBackColor = true;
-			// 
-			// btn_payments
-			// 
-			this->btn_payments->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->btn_payments->Location = System::Drawing::Point(243, 4);
-			this->btn_payments->Name = L"btn_payments";
-			this->btn_payments->Size = System::Drawing::Size(75, 23);
-			this->btn_payments->TabIndex = 3;
-			this->btn_payments->Text = L"Pagos";
-			this->btn_payments->UseVisualStyleBackColor = true;
+			this->btn_users->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->btn_users->Location = System::Drawing::Point(405, 4);
+			this->btn_users->Name = L"btn_users";
+			this->btn_users->Size = System::Drawing::Size(75, 23);
+			this->btn_users->TabIndex = 5;
+			this->btn_users->Text = L"Usuarios";
+			this->btn_users->UseVisualStyleBackColor = true;
+			this->btn_users->Click += gcnew System::EventHandler(this, &Reportes::showUsersReport);
 			// 
 			// btn_inventary
 			// 
@@ -167,15 +148,45 @@ namespace QuickShop {
 			this->btn_inventary->Text = L"Inventario";
 			this->btn_inventary->UseVisualStyleBackColor = true;
 			// 
-			// btn_users
+			// btn_payments
 			// 
-			this->btn_users->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->btn_users->Location = System::Drawing::Point(405, 4);
-			this->btn_users->Name = L"btn_users";
-			this->btn_users->Size = System::Drawing::Size(75, 23);
-			this->btn_users->TabIndex = 5;
-			this->btn_users->Text = L"Usuarios";
-			this->btn_users->UseVisualStyleBackColor = true;
+			this->btn_payments->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->btn_payments->Location = System::Drawing::Point(243, 4);
+			this->btn_payments->Name = L"btn_payments";
+			this->btn_payments->Size = System::Drawing::Size(75, 23);
+			this->btn_payments->TabIndex = 3;
+			this->btn_payments->Text = L"Pagos";
+			this->btn_payments->UseVisualStyleBackColor = true;
+			// 
+			// btn_purchases
+			// 
+			this->btn_purchases->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->btn_purchases->Location = System::Drawing::Point(162, 4);
+			this->btn_purchases->Name = L"btn_purchases";
+			this->btn_purchases->Size = System::Drawing::Size(75, 23);
+			this->btn_purchases->TabIndex = 2;
+			this->btn_purchases->Text = L"Compras";
+			this->btn_purchases->UseVisualStyleBackColor = true;
+			// 
+			// btn_products
+			// 
+			this->btn_products->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->btn_products->Location = System::Drawing::Point(81, 4);
+			this->btn_products->Name = L"btn_products";
+			this->btn_products->Size = System::Drawing::Size(75, 23);
+			this->btn_products->TabIndex = 1;
+			this->btn_products->Text = L"Catalogo";
+			this->btn_products->UseVisualStyleBackColor = true;
+			// 
+			// btn_clients
+			// 
+			this->btn_clients->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->btn_clients->Location = System::Drawing::Point(0, 4);
+			this->btn_clients->Name = L"btn_clients";
+			this->btn_clients->Size = System::Drawing::Size(75, 23);
+			this->btn_clients->TabIndex = 0;
+			this->btn_clients->Text = L"Clientes";
+			this->btn_clients->UseVisualStyleBackColor = true;
 			// 
 			// panel_table
 			// 
@@ -187,10 +198,13 @@ namespace QuickShop {
 			// 
 			// dgv_report
 			// 
+			this->dgv_report->AllowUserToAddRows = false;
+			this->dgv_report->AllowUserToDeleteRows = false;
 			this->dgv_report->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			this->dgv_report->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->dgv_report->Location = System::Drawing::Point(0, 0);
 			this->dgv_report->Name = L"dgv_report";
+			this->dgv_report->ReadOnly = true;
 			this->dgv_report->Size = System::Drawing::Size(666, 322);
 			this->dgv_report->TabIndex = 0;
 			// 
@@ -214,5 +228,66 @@ namespace QuickShop {
 
 		}
 #pragma endregion
-	};
+	private: System::Void showUsersReport(System::Object^ sender, System::EventArgs^ e) {
+		this->dgv_report->Rows->Clear();
+		this->dgv_report->Columns->Add("id_customer", "ID");
+		this->dgv_report->Columns->Add("name", "Nombre");
+		this->dgv_report->Columns->Add("lastName", "Apellido");
+		this->dgv_report->Columns->Add("user", "Usuario");
+		this->dgv_report->Columns->Add("typeUser", "Tipo de Usuario");
+		this->dgv_report->Columns->Add("cui", "CUI / Pasaporte");
+		this->dgv_report->Columns->Add("phoneNum", "Número de Teléfono");
+		this->dgv_report->Columns->Add("email", "Correo Electrónico");
+		this->dgv_report->Columns->Add("address", "Dirección Domiciliar");
+
+		ifstream usuaa("users.csv");
+
+		if (!usuaa.is_open()) {
+			MessageBox::Show("Error al abrir el archivo");
+		}
+		else {
+			string line;
+			int limit = 0;
+			while (getline(usuaa, line)) {
+				Cstomer^ newUser = gcnew Cstomer();
+				string id;
+				string username;
+				string type;
+				string cui;
+				string name;
+				string lastname;
+				string address;
+				string phonenumb;
+				string email;
+				string password;
+
+				stringstream ss(line);
+				getline(ss, id, ',');
+				getline(ss, name, ',');
+				getline(ss, lastname, ',');
+				getline(ss, username, ',');
+				getline(ss, type, ',');
+				getline(ss, cui, ',');
+				getline(ss, phonenumb, ',');
+				getline(ss, email, ',');
+				getline(ss, address, ',');
+				getline(ss, password, ',');
+				newUser->id_customer = gcnew String(id.c_str());
+				newUser->name = gcnew String(name.c_str());
+				newUser->lastName = gcnew String(lastname.c_str());
+				newUser->username = gcnew String(username.c_str());
+				newUser->type = gcnew String(type.c_str());
+				newUser->cui = gcnew String(cui.c_str());
+				newUser->phoneNum = gcnew String(phonenumb.c_str());
+				newUser->email = gcnew String(email.c_str());
+				newUser->address = gcnew String(address.c_str());
+				newUser->password = gcnew String(password.c_str());
+
+				localData[limit] = newUser;
+				this->dgv_report->Rows->Add(localData[limit]->id_customer, localData[limit]->name, localData[limit]->lastName, localData[limit]->username, localData[limit]->type, localData[limit]->cui, localData[limit]->phoneNum, localData[limit]->email, localData[limit]->address);
+				limit++;
+			}
+		}
+	}
+};
 }
