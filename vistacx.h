37b54f32d338ce5eba7c1d22366157784596f212
Javier Barillas <nefy.rios@claro.com.gt>
2024-05-19 17:36:12 -0600

    Maqueta de dashboard CX

--- vistacx.h
+++ vistacx.h
@@ -34,6 +34,10 @@ namespace QuickShop {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::FlowLayoutPanel^ navbar;
+	protected:
+	private: System::Windows::Forms::FlowLayoutPanel^ sidebar;
+	private: System::Windows::Forms::Panel^ panel_cont;
 
 	private:
 		/// <summary>
@@ -48,11 +52,47 @@ namespace QuickShop {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"vistacx";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->navbar = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->sidebar = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->panel_cont = (gcnew System::Windows::Forms::Panel());
+			this->SuspendLayout();
+			// 
+			// navbar
+			// 
+			this->navbar->Dock = System::Windows::Forms::DockStyle::Top;
+			this->navbar->Location = System::Drawing::Point(0, 0);
+			this->navbar->Name = L"navbar";
+			this->navbar->Size = System::Drawing::Size(1978, 100);
+			this->navbar->TabIndex = 0;
+			// 
+			// sidebar
+			// 
+			this->sidebar->Dock = System::Windows::Forms::DockStyle::Left;
+			this->sidebar->Location = System::Drawing::Point(0, 100);
+			this->sidebar->Name = L"sidebar";
+			this->sidebar->Size = System::Drawing::Size(200, 960);
+			this->sidebar->TabIndex = 1;
+			// 
+			// panel_cont
+			// 
+			this->panel_cont->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel_cont->Location = System::Drawing::Point(200, 100);
+			this->panel_cont->Name = L"panel_cont";
+			this->panel_cont->Size = System::Drawing::Size(1778, 960);
+			this->panel_cont->TabIndex = 2;
+			// 
+			// vistacx
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(16, 31);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1978, 1060);
+			this->Controls->Add(this->panel_cont);
+			this->Controls->Add(this->sidebar);
+			this->Controls->Add(this->navbar);
+			this->Name = L"vistacx";
+			this->Text = L"vistacx";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
